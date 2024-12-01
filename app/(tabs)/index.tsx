0e7d442a3446460f98587ef93fba0223ab0a7f85
thiagonees <thiagoalves.devp@gmail.com>
2024-12-01 10:40:18 -0300

    🎉 Initial commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from '@/components/Themed';
+
+export default function TabOneScreen() {
+  return (
+    <View className='flex-1 justify-center  items-center'>
+      <Text >Tab One</Text>
+    </View>
+
+  );
+}
+
